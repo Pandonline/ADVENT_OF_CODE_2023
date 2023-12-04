137afa31d9d89e529b07810fbdd2a0b9be09c84b
Pandadorable <geffroy250@gmail.com>
2023-12-04 12:50:51 +0100

    Day4 partie2

--- Day4/Scratchcards.py
+++ Day4/Scratchcards.py
@@ -1,25 +1,29 @@
 with open("input.txt") as input:
     
-    sum = 0
-    
-    for line in input.readlines():
+    L = input.readlines()
+    scr = [1 for i in range(len(L))]
+    for line in L:
         win,number = line.strip("\n").split("|")
         card,win = win.split(":")
-        
+        nbCard = int(card.split(" ")[-1])
+        print(nbCard)
         win = win.split(" ")
         number = number.split(" ")
         
         win = [int(i) for i in win if (i != " " and i != "")]
         number = [int(i) for i in number if (i != " " and i != "")]
-        
+                
         score = 0
 
         for i in number:
             if i in win:
-                if score == 0:
-                    score = 1
-                else:
-                    score = 2*score
-        
-        sum = sum + score
+                score = score +1
+        i = 0
+        while score > 0:
+            scr[nbCard+i] = scr[nbCard+i] +scr[nbCard-1]
+            i = i +1
+            score = score -1
+    sum = 0
+    for i in scr:
+        sum = sum + i
     print(sum)

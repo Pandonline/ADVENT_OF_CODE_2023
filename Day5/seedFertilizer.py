482495dbc352e606c01058afe4721d8877424e85
Pandonline <95920019+Pandonline@users.noreply.github.com>
2023-12-05 20:54:39 +0100

    Day5 partie2

--- Day5/seedFertilizer.py
+++ Day5/seedFertilizer.py
@@ -1,5 +1,6 @@
 with open("input.txt") as input:
-    listSeed = [ int(i) for i in (input.readline().strip("\n").split(": "))[1].split(" ") ]
+    L = (input.readline().strip("\n").split(": "))[1].split(" ")
+    listSeed = [ (int(L[2*i]),int(L[2*i+1])) for i in range(len(L)//2)  ]
     print("listSeed : ",listSeed)
     input.readline() ##empty line
     
@@ -10,12 +11,39 @@ with open("input.txt") as input:
         while not (L == "\n" or L == ""):
             map.append( [ int(i) for i in L.strip("\n").split(" ") ] )             
             L = input.readline()
-        
+        newList = []
         for i in range(len(listSeed)):
+            cut = False
             for m in map:
-                ecart = listSeed[i] - m[1]
-                if ecart >= 0 and ecart < m[2]:
-                    listSeed[i] = m[0]+ecart
+                seed_start = listSeed[i][0]
+                seed_end = listSeed[i][0] + listSeed[i][1] -1
+                seed_size = listSeed[i][1]
+                
+                search_start = m[1]
+                search_end = m[1]+m[2]-1
+                
+                convert_start = m[0]
+                convert_end = m[0]+m[2]-1
+                
+                if seed_start > search_end:
+                    pass
+                elif seed_start < search_start:
+                    if seed_end < search_start:
+                        pass
+                    elif seed_end >= search_start:
+                        cut = True
+                        newList.append((seed_start,search_start-seed_start))
+                        newList.append((convert_start,min(seed_end-search_start+1,m[2])))
+                        if seed_end > search_end:
+                            newList.append((search_end+1,seed_end-search_end+1))
+                        break
+                elif seed_start >= search_start:
+                    cut = True
+                    newList.append((convert_start+seed_start-search_start,min(seed_size,search_end-seed_start+1)))
+                    if seed_end > search_end:
+                        newList.append((search_end+1,seed_end-search_end))
                     break
-    
+            if not cut:
+                newList.append((listSeed[i][0],listSeed[i][1]))
+        listSeed = newList
     print(min(listSeed))

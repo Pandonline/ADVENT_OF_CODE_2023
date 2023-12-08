5f631bdacbcec14472e506dce8ce996b6348dec5
Pandonline <95920019+Pandonline@users.noreply.github.com>
2023-12-08 06:46:52 +0100

    Day8 partie2

--- Day8/wasteland.py
+++ Day8/wasteland.py
@@ -1,3 +1,5 @@
+import math
+
 with open("input.txt") as input :
     readTrajet = True
     trajet = ""
@@ -12,14 +14,24 @@ with open("input.txt") as input :
         path,way = L.strip("\n").split(" = ")
         left,right = (way.replace("(","").replace(")","").replace(",","")).split(" ")
         DICO[path] = (left,right)
-        
-    node = 'AAA'
-    sum = 0
-    while not node == "ZZZ":
-        if trajet[sum%len(trajet)] == "L":
-            node = DICO[node][0]
-        elif trajet[sum%len(trajet)] == "R":
-            node = DICO[node][1]
-        sum = sum + 1
-    print(sum)
+      
+    listStart = []
+    for key in DICO.keys():
+        if key[2] == 'A':
+            listStart.append(key)
+            
+    listSum = []
+    for i in listStart:
+        sum = 0
+        node = i
+        while not node[2] == 'Z':
+            if trajet[sum%len(trajet)] == "L":
+                node = DICO[node][0]
+            elif trajet[sum%len(trajet)] == "R":
+                node = DICO[node][1]
+            sum = sum +1
+        listSum.append(sum)
+    print(listSum)
+    s= math.lcm(*listSum)
+    print(s)
     

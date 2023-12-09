b668fb979e883f2c6d38290537e303aba08ed047
Pandonline <95920019+Pandonline@users.noreply.github.com>
2023-12-09 11:29:35 +0100

    Day9 partie2

--- Day9/Mirage.py
+++ Day9/Mirage.py
@@ -5,16 +5,16 @@ with open("input.txt") as input:
         list.append([int(i) for i in L.strip("\n").split(" ")])
         allZero = False
         i = 0
-        lastValue = [list[i][-1]]
+        firstValue = [list[i][0]]
         while not allZero :
             nList = [list[i][j+1]-list[i][j] for j in range(len(list[i])-1)]
-            lastValue.append(nList[-1])
+            firstValue.append(nList[0])
             allZero = nList.count(0) == len(nList)
             list.append(nList)
             i = i+1
         newValue = [0]
-        for j in range(len(lastValue)-1):
-            newValue.append(lastValue[-j-2]+newValue[j])
+        for j in range(len(firstValue)-1):
+            newValue.append(firstValue[-j-2]-newValue[j])
         sum = sum + newValue[-1]
     print(sum)
             

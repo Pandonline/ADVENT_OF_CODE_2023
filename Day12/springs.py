382ae918ecc2fbe4c057796296a9ab4913d2dcfe
Pandadorable <geffroy250@gmail.com>
2023-12-12 15:03:13 +0100

    Day12 partie2

--- Day12/springs.py
+++ Day12/springs.py
@@ -1,3 +1,10 @@
+from functools import *
+
+CACHE = {}
+
+def genKey(sp,num):
+    return sp+str(num)
+
 def convert(sp,val):
     s = ""
     k = 0
@@ -10,27 +17,26 @@ def convert(sp,val):
     return s
 
 def verif(sp,num):
+    
     sum = 0
     for k in num:
         sum = sum+k
     if not sum == sp.count("#"):
+        CACHE[key] = False
         return False
     else:
         l = [i for i in sp.split(".") if not i == '']
         j = 0
         for i in l:
             if not len(i) == num[j]:
+                CACHE[key] = False
                 return False
             j = j+1
+    CACHE[key] = True
     return True
-sum = 0
-input = open("input.txt")
-for L in input.readlines():
-    print(L)
-    springs,num = L.strip("\n").split(" ")
-    num = [int(n) for n in num.split(",")]
-    
-    nbInterogateur = springs.count("?")
+
+@cache
+def ListBinaire(nbInterogateur):
     listVal = []
     for i in range(2**nbInterogateur):
         val = []
@@ -42,9 +48,65 @@ for L in input.readlines():
                 val.append(".")
             tmp = tmp//2
         listVal.append(val)
-    combi = 0
-    for i in listVal:
-        if verif(convert(springs,i),num):
-            combi = combi+1
-    sum = sum + combi
+    return listVal
+    
+def recur(str,num):
+    key = genKey(str,num)
+    if key in CACHE:
+        return CACHE[key]
+    
+    if len(str) == 0 and len(num) == 0:
+        CACHE[key] = 1
+        return 1
+    if len(str) == 0 and not len(num) == 0:
+        CACHE[key] = 0
+        return 0
+    if str[0] == '.':
+        tmp = recur(str[1:],num)
+        CACHE[key] = tmp
+        return tmp
+    if str[0] == '?':
+        tmp = 0
+        tmp = tmp + recur('#'+str[1:],num)
+        tmp = tmp + recur('.'+str[1:],num)
+        CACHE[key] = tmp
+        return tmp
+    if str[0] == '#':
+        if len(num) == 0 or num[0]>len(str):
+            CACHE[key] = 0
+            return 0
+        for i in range(num[0]):
+            if str[i] == '.':
+                CACHE[key] = 0 
+                return 0
+        if len(str) == num[0]:    
+            if len(num) == 1:
+                CACHE[key] = 1
+                return 1   
+            else:
+                CACHE[key] = 0
+                return 0
+        else:
+            if str[num[0]] == '#':
+                CACHE[key] = 0
+                return 0
+            n_str = str[num[0]:]
+            if str[num[0]] == '?':
+                n_str = '.' + str[num[0]+1:]
+            tmp = recur(n_str,num[1:])
+            CACHE[key] = tmp
+            return tmp
+    return 0
+
+sum = 0
+input = open("input.txt")
+for L in input.readlines():
+    
+    springs,num = L.strip("\n").split(" ")
+    num = [int(n) for n in num.split(",")]
+    
+    springs = springs+'?'+springs+'?'+springs+'?'+springs+'?'+springs
+    num = num+num+num+num+num
+    
+    sum = sum + recur(springs,num)
 print(sum)

4995bd07baaafa91049bc9cd74b95730adfc481d
Pandonline <95920019+Pandonline@users.noreply.github.com>
2023-12-13 12:44:54 +0100

    Day13 partie2

--- Day13/mirrors.py
+++ Day13/mirrors.py
@@ -1,27 +1,37 @@
-def verifSymetrieLine(MAP,l1,l2,start):
+def verifSymetrieLine(MAP,l1,l2,start,smudge):
     if  l1 < 0 or  l2 >= len(MAP):
+        if not smudge == 0:
+            return 0
         return start
     for j in range(len(MAP[0])):
         if not MAP[l1][j] == MAP[l2][j]:
-            return 0
-    return verifSymetrieLine(MAP,l1-1,l2+1,start)
+            if smudge > 0:
+                smudge -= 1
+            else:
+                return 0
+    return verifSymetrieLine(MAP,l1-1,l2+1,start,smudge)
 
-def verifSymetrieCol(MAP,c1,c2,start):
+def verifSymetrieCol(MAP,c1,c2,start,smudge):
     if  c1 < 0 or  c2 >= len(MAP[0]):
+        if not smudge == 0:
+            return 0
         return start
     for i in range(len(MAP)):
         if not MAP[i][c1] == MAP[i][c2]:
-            return 0
-    return verifSymetrieCol(MAP,c1-1,c2+1,start)
+            if smudge > 0:
+                smudge -= 1
+            else:
+                return 0
+    return verifSymetrieCol(MAP,c1-1,c2+1,start,smudge)
                 
 
 def findSymetrie(MAP):
     nbLine = 0
     nbColonne = 0
     for i in range(len(MAP)-1):
-        nbLine += verifSymetrieLine(MAP,i,i+1,i+1)
+        nbLine += verifSymetrieLine(MAP,i,i+1,i+1,1)
     for j in range(len(MAP[0])-1):
-        nbColonne += verifSymetrieCol(MAP,j,j+1,j+1)
+        nbColonne += verifSymetrieCol(MAP,j,j+1,j+1,1)
     return 100*nbLine+nbColonne
 
 
